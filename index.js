0000bb27178d0cf06b4759edafd16325a293ddb0
Jonathan Aguilar <j.aguilar0390@gmail.com>
2022-04-26 21:07:56 -0400

    commit tree e01c76c6223d74c4a335a044fea4cf916b9616d8

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+    return cats;
+}
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+    return cats;
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+function appendCat(name) {
+    const copyCats = [...cats, 'Broom'];
+    return copyCats;
+}
+function prependCat(name) {
+    const copyCats1 = ['Arnold', ...cats];
+    return copyCats1
+}
+function removeLastCat() {
+    const copyCats2 = cats.slice(0, cats.length - 1);
+    return copyCats2;
+}
+function removeFirstCat() {
+    const copyCats2 = cats.slice(1);
+    return copyCats2;
+}
